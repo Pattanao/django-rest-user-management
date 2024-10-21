9618a869bdf44a8b5a2e62b65d151ece28fea6af
Pattanao <pat.sutanc@gmail.com>
2024-10-21 21:50:58 +0700

    feat(users): move Profile validator logic to serializer

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator, RegexValidator
 from django_countries.serializers import CountryFieldMixin
 from rest_framework import serializers
 
@@ -12,7 +13,21 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class ProfileSerializer(CountryFieldMixin, serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
+    user = UserSerializer(read_only=False)
+    age = serializers.IntegerField(
+        validators=[
+            MinValueValidator(18, message="Age must greater than 18."),
+            MaxValueValidator(85, message="Age must less than 85."),
+        ]
+    )
+    phone_number = serializers.CharField(
+        validators=[
+            RegexValidator(
+                regex=r"^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$",
+                message="Phone Number must be in valid format.",
+            )
+        ],
+    )
 
     class Meta:
         model = Profile
